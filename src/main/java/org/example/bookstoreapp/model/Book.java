00ad0b0c048e19b26912545063edf215d2d5a019
Kamil Szerszen <szerszenk1997@gmail.com>
2025-07-18 11:02:45 +0200

    Create BookRepository, application.properties config

--- src/main/java/org/example/bookstoreapp/model/Book.java
+++ src/main/java/org/example/bookstoreapp/model/Book.java
@@ -1,4 +1,24 @@
 package org.example.bookstoreapp.model;
 
-public class book {
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false)
+    private String isbn;
+    @Column(nullable = false)
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
 }
--- src/main/java/org/example/bookstoreapp/repository/impl/BookRepositoryImpl.java
+++ src/main/java/org/example/bookstoreapp/repository/impl/BookRepositoryImpl.java
@@ -1,4 +1,17 @@
 package org.example.bookstoreapp.repository.impl;
 
-public class BookRepositoryImpl {
+import java.util.List;
+import org.example.bookstoreapp.model.Book;
+import org.example.bookstoreapp.repository.BookRepository;
+
+public class BookRepositoryImpl implements BookRepository {
+    @Override
+    public Book save(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return List.of();
+    }
 }
