a4891426ee821f35eabcd3f02511c98c191e65f2
Kamil Szerszen <szerszenk1997@gmail.com>
2025-07-25 14:11:12 +0200

    Add JpaRepository delete BookRepositoryImpl

--- src/main/java/org/example/bookstoreapp/repository/BookRepository.java
+++ src/main/java/org/example/bookstoreapp/repository/BookRepository.java
@@ -3,8 +3,9 @@ package org.example.bookstoreapp.repository;
 import java.util.List;
 import java.util.Optional;
 import org.example.bookstoreapp.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book save(Book book);
 
