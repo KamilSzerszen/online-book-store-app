3a403d90c0683d98ff929cf743db2942213c7117
Kamil Szerszen <szerszenk1997@gmail.com>
2025-07-25 15:10:20 +0200

    Add Liquibase, create table books

--- src/main/java/org/example/bookstoreapp/model/Book.java
+++ src/main/java/org/example/bookstoreapp/model/Book.java
@@ -17,18 +17,23 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull(message = "Title cannot be null")
     @Column(nullable = false)
     private String title;
+
     @NotNull(message = "Author cannot be null")
     @Column(nullable = false)
     private String author;
+
     @NotNull(message = "Isbn cannot be null")
     @Column(nullable = false)
     private String isbn;
+
     @NotNull(message = "Price cannot be null")
     @Column(nullable = false)
     private BigDecimal price;
+
     private String description;
     private String coverImage;
 }
--- src/main/java/org/example/bookstoreapp/repository/BookRepository.java
+++ src/main/java/org/example/bookstoreapp/repository/BookRepository.java
@@ -1,15 +1,10 @@
 package org.example.bookstoreapp.repository;
 
-import java.util.List;
-import java.util.Optional;
 import org.example.bookstoreapp.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Book save(Book book);
-
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
 }
