918a8e890826ed3b1e5ba51bf204c04bef9a78ff
Kamil Szerszen <szerszenk1997@gmail.com>
2025-08-12 17:44:47 +0200

    Create UserService, Userrepository and methods

--- src/main/java/org/example/bookstoreapp/controller/AuthenticationController.java
+++ src/main/java/org/example/bookstoreapp/controller/AuthenticationController.java
@@ -1,15 +1,20 @@
 package org.example.bookstoreapp.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.example.bookstoreapp.dto.user.UserRegistrationRequestDto;
 import org.example.bookstoreapp.dto.user.UserResponseDto;
+import org.example.bookstoreapp.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Authentication", description = "User authentication and authorization operations")
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final UserService userService;
 
     public UserResponseDto register(UserRegistrationRequestDto request) {
         return userService.register(request);
--- src/main/java/org/example/bookstoreapp/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/bookstoreapp/service/impl/UserServiceImpl.java
@@ -1,13 +1,25 @@
 package org.example.bookstoreapp.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.example.bookstoreapp.dto.user.UserRegistrationRequestDto;
 import org.example.bookstoreapp.dto.user.UserResponseDto;
+import org.example.bookstoreapp.mapper.UserMapper;
+import org.example.bookstoreapp.model.User;
+import org.example.bookstoreapp.repository.user.UserRepository;
 import org.example.bookstoreapp.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto request) {
-        return null;
+        User user = userMapper.toModel(request);
+        User registered = userRepository.save(user);
+        return userMapper.toDto(registered);
     }
 }
